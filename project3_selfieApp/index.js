46662c443fb3081ffdd5556a0d39440b6b2ddac7
Yannick Lansink <yannick.lansink@live.nl>
2021-06-27 13:39:12 +0200

    added database

--- project3_selfieApp/index.js
+++ project3_selfieApp/index.js
@@ -1,15 +1,23 @@
 
 const express = require('express');
+const Datastore = require('nedb')
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 app.post('/api', (request, response) => {
-    console.log(request.body);
+    console.log('I got a request')
     const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
     response.json({
         status: 'success',
+        timestamp: timestamp,
         latitude: data.lat, 
         longitude: data.lon,
     });
