31133abe96631ab9b61cb9155944084572ba8721
Yannick Lansink <yannick.lansink@live.nl>
2021-06-27 23:01:20 +0200

    d

--- project3_selfieApp/index.js
+++ project3_selfieApp/index.js
@@ -9,16 +9,20 @@ app.use(express.json({ limit: '1mb' }));
 const database = new Datastore('database.db');
 database.loadDatabase();
 
+app.get('/api', (request, response) => {
+    database.find({}, (err, data) => {
+        if (err) {
+            response.end();
+            return;
+        };
+        response.json(data);
+    });
+});
+
 app.post('/api', (request, response) => {
-    console.log('I got a request')
     const data = request.body;
     const timestamp = Date.now();
     data.timestamp = timestamp;
     database.insert(data);
-    response.json({
-        status: 'success',
-        timestamp: timestamp,
-        latitude: data.lat, 
-        longitude: data.lon,
-    });
-});
+    response.json(data);
+  });
